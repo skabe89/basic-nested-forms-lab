b14f5eed9c5ba2e744411f4cc6d83dd6c2849e34
Sean <skabe89@gmail.com>
2021-01-20 11:53:28 -0500

    commit tree 3eadc4be6470675b57fc0494ce2d4c6092c6230a

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+   
   end
 
   def index
@@ -9,8 +10,23 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    recipeone = @recipe.ingredients.build
+    recipetwo = @recipe.ingredients.build
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    # byebug
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
